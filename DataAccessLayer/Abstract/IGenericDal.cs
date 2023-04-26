292b49a95fef4617edaf589d36e247c10f9dcc9c
erhangndz <erhangndz@live.com>
2023-04-26 21:32:44 +0300

    Updated

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        public void Insert(T t);
+        public void Update(T t);
+        public void Delete(T t);
+        T  GetByID(int id);
+        List<T> GetList();
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context _context;
+
+        
+
+        public void Delete(T t)
+        {
+           _context.Remove(t);
+           _context.SaveChanges(); 
+        }
+
+        public T GetByID(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            _context.Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _context.Update(t);
+            _context.SaveChanges();
+        }
+    }
+}
